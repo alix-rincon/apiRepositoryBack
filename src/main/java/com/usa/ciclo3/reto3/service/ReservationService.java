22a89b2383d83313fe25b9a22090170985e43b67
Alix Rincón Carreño <alixshayanna@gmail.com>
2021-10-30 12:11:21 -0500

    Avances reto 5

--- src/main/java/com/usa/ciclo3/reto3/service/ReservationService.java
+++ src/main/java/com/usa/ciclo3/reto3/service/ReservationService.java
@@ -1,7 +1,14 @@
 package com.usa.ciclo3.reto3.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import com.usa.ciclo3.reto3.helpers.ContadorClient;
+import com.usa.ciclo3.reto3.helpers.StatusReservas;
 import com.usa.ciclo3.reto3.model.Reservation;
 import com.usa.ciclo3.reto3.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -89,5 +96,31 @@ public class ReservationService {
         }).orElse(false);
         return aBoolean;
     }
-    
+
+    public StatusReservas reporteStatusServicio() {
+        List<Reservation> completed = reservationRepository.ReservacionStatusRepositorio("completed");
+        List<Reservation> cancelled = reservationRepository.ReservacionStatusRepositorio("cancelled");
+        return new StatusReservas(completed.size(), cancelled.size());
+    }
+
+    public List<Reservation> reporteTiempoServicio(String datoA, String datoB) {
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+        Date datoUno = new Date();
+        Date datoDos = new Date();
+        try {
+            datoUno = parser.parse(datoA);
+            datoDos = parser.parse(datoB);
+        } catch (ParseException evt) {
+            evt.printStackTrace();
+        }
+        if (datoUno.before(datoDos)) {
+            return reservationRepository.ReservacionTiempoRepositorio(datoUno, datoDos);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    public List<ContadorClient> reporteClientesServicio() {
+        return reservationRepository.getClientesRepositorio();
+    }
 }
